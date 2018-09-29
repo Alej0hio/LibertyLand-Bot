5f2c836f59e9e2a4fa6aa709fc5ab2c06a72792a
Marko <32610623+Apfel@users.noreply.github.com>
2018-09-29 19:57:27 +0200

    fixed imports

--- src/main/java/tk/liblnd/bot/commands/discord/ListCmd.java
+++ src/main/java/tk/liblnd/bot/commands/discord/ListCmd.java
@@ -2,6 +2,8 @@ package tk.liblnd.bot.commands.discord;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import tk.liblnd.bot.LLBot;
 
